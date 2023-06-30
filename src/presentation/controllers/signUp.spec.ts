d2e89a083f8204f4785a697a4961d24551b211ce
Luan Bianchini <luanbianchine1@gmail.com>
2023-06-30 00:19:29 -0300

    feat: ensure the SignUpController return error if missing email

--- src/presentation/controllers/signUp.spec.ts
+++ src/presentation/controllers/signUp.spec.ts
@@ -15,4 +15,18 @@ describe('Sign Up controller', () => {
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new Error('missing param: name'))
   })
+  test('should return 400 if no email is provided', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: 'Teste',
+        // email: 'teste_email@gmail.com',
+        password: 'pass',
+        passwordConfirmation: 'pass'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('missing param: email'))
+  })
 })
