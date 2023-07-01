6e2a1cad92b151a261df6d6937d9dfd1add270b2
Luan Bianchini <luanbianchine1@gmail.com>
2023-07-01 17:29:15 -0300

    test: ensure dbAddAccount throws if encrypter throws

--- src/data/usecases/addAccount/db.add.account.spec.ts
+++ src/data/usecases/addAccount/db.add.account.spec.ts
@@ -36,4 +36,15 @@ describe('DbAddAccount Usecase', () => {
     await sut.add(accountData)
     expect(encryptSpy).toHaveBeenCalledWith('valid_password')
   })
+  test('Should throw if Encrypter throws', async () => {
+    const { encrypterStub, sut } = makeSut()
+    jest.spyOn(encrypterStub, 'encrypt').mockReturnValueOnce(Promise.reject(new Error()))
+    const accountData = {
+      name: 'valid_name',
+      email: 'valid_email',
+      password: 'valid_pass'
+    }
+    const promise = sut.add(accountData)
+    await expect(promise).rejects.toThrow()
+  })
 })
