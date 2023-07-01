b25d01e9cbe779b31183d808a6d80b92ae616e55
Luan Bianchini <luanbianchine1@gmail.com>
2023-07-01 11:46:54 -0300

    refactor: improve helper serverError method

--- src/presentation/controllers/signUp.ts
+++ src/presentation/controllers/signUp.ts
@@ -4,7 +4,6 @@ import { badRequest, serverError } from '../helpers/http.helper'
 import { type Controller } from '../protocols/controller'
 import { type EmailValidator } from '../protocols/email.validator'
 import { InvalidParamError } from '../errors/invalid.param.error'
-import { ServerError } from '../errors/server.error'
 
 export class SignUpController implements Controller {
   constructor (private readonly emailValidator: EmailValidator) {
@@ -22,7 +21,7 @@ export class SignUpController implements Controller {
       const isValid = this.emailValidator.isValid(httpRequest.body.email)
       if (!isValid) return badRequest(new InvalidParamError('email'))
     } catch (error) {
-      return serverError(new ServerError())
+      return serverError()
     }
   }
 }
