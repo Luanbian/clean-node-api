2dfa140c63b9a2e622e2ea112ef446ef274ba02d
Luan Bianchini <luanbianchine1@gmail.com>
2023-07-01 11:43:10 -0300

    test: case of server error

--- src/presentation/controllers/signUp.spec.ts
+++ src/presentation/controllers/signUp.spec.ts
@@ -2,6 +2,7 @@ import { SignUpController } from './signUp'
 import { MissingParamError } from '../errors/missing.param.error'
 import { InvalidParamError } from '../errors/invalid.param.error'
 import { type EmailValidator } from '../protocols/email.validator'
+import { ServerError } from '../errors/server.error'
 
 interface SutTypes {
   sut: SignUpController
@@ -104,5 +105,25 @@ describe('Sign Up controller', () => {
     }
     sut.handle(httpRequest)
     expect(isValidSpy).toHaveBeenCalledWith('invalid_email')
+  })
+  test('should return 500 if EmailValidator throws', () => {
+    class EmailValidatorStub implements EmailValidator {
+      isValid (email: string): boolean {
+        throw new Error()
+      }
+    }
+    const emailValidatorStub = new EmailValidatorStub()
+    const sut = new SignUpController(emailValidatorStub)
+    const httpRequest = {
+      body: {
+        name: 'Teste',
+        email: 'invalid_email',
+        password: 'pass',
+        passwordConfirmation: 'pass'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse?.statusCode).toBe(500)
+    expect(httpResponse?.body).toEqual(new ServerError())
   })
 })
--- src/presentation/controllers/signUp.ts
+++ src/presentation/controllers/signUp.ts
@@ -1,9 +1,10 @@
 import { MissingParamError } from '../errors/missing.param.error'
 import { type HttpRequest, type HttpResponse } from '../protocols/http'
-import { badRequest } from '../helpers/http.helper'
+import { badRequest, serverError } from '../helpers/http.helper'
 import { type Controller } from '../protocols/controller'
 import { type EmailValidator } from '../protocols/email.validator'
 import { InvalidParamError } from '../errors/invalid.param.error'
+import { ServerError } from '../errors/server.error'
 
 export class SignUpController implements Controller {
   constructor (private readonly emailValidator: EmailValidator) {
@@ -11,13 +12,17 @@ export class SignUpController implements Controller {
   }
 
   handle (httpRequest: HttpRequest): HttpResponse | undefined {
-    const requiredFields = ['email', 'name', 'password', 'passwordConfirmation']
-    for (const field of requiredFields) {
-      if (!httpRequest.body[field]) {
-        return badRequest(new MissingParamError(field))
+    try {
+      const requiredFields = ['email', 'name', 'password', 'passwordConfirmation']
+      for (const field of requiredFields) {
+        if (!httpRequest.body[field]) {
+          return badRequest(new MissingParamError(field))
+        }
       }
+      const isValid = this.emailValidator.isValid(httpRequest.body.email)
+      if (!isValid) return badRequest(new InvalidParamError('email'))
+    } catch (error) {
+      return serverError(new ServerError())
     }
-    const isValid = this.emailValidator.isValid(httpRequest.body.email)
-    if (!isValid) return badRequest(new InvalidParamError('email'))
   }
 }
