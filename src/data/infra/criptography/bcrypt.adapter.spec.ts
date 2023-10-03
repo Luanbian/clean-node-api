70f78cc54469ffeb707b0551ee40219e274d75e0
Luan Bianchini <luanbianchine1@gmail.com>
2023-10-03 13:59:49 -0300

    test: error case

--- src/data/infra/criptography/bcrypt.adapter.spec.ts
+++ src/data/infra/criptography/bcrypt.adapter.spec.ts
@@ -24,4 +24,12 @@ describe('Bcrypt adapter', () => {
     const hash = await sut.encrypt('any_value')
     expect(hash).toBe('encripted_hash')
   })
+  test('Should throw if bcrypt throws', async () => {
+    const sut = makeSut()
+    jest.spyOn(bcrypt, 'hash').mockImplementationOnce(() => {
+      throw new Error()
+    })
+    const promise = sut.encrypt('any_value')
+    await expect(promise).rejects.toThrow()
+  })
 })
