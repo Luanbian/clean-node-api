c78df201222c29b11f0c9eb6b7b1d63469e1cdd7
Luan Bianchini <luanbianchine1@gmail.com>
2023-10-03 11:43:46 -0300

    feat: return hashed on success

--- src/data/infra/criptography/bcrypt.adapter.spec.ts
+++ src/data/infra/criptography/bcrypt.adapter.spec.ts
@@ -1,6 +1,12 @@
 import bcrypt from 'bcrypt'
 import { BcryptAdapter } from './bcrypt.adapter'
 
+jest.mock('bcrypt', () => ({
+  async hash (): Promise<string> {
+    return await new Promise(resolve => { resolve('encripted_hash') })
+  }
+}))
+
 describe('Bcrypt adapter', () => {
   test('Shoul call Bcrypt with correct value', async () => {
     const salt = 12
@@ -8,5 +14,11 @@ describe('Bcrypt adapter', () => {
     const hashSpy = jest.spyOn(bcrypt, 'hash')
     await sut.encrypt('any_value')
     expect(hashSpy).toHaveBeenLastCalledWith('any_value', salt)
+  })
+  test('Shoul return a hash on success', async () => {
+    const salt = 12
+    const sut = new BcryptAdapter(salt)
+    const hash = await sut.encrypt('any_value')
+    expect(hash).toBe('encripted_hash')
   })
 })
